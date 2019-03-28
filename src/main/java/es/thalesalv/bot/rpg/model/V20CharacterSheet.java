915d0670e64fd4ae4ff6620b5dc5d7f1c047d135
Thales Alves <thales.pereira@dell.com>
2019-03-28 18:06:08 -0300

    adicionados itens na ficha

--- src/main/java/es/thalesalv/bot/rpg/model/V20CharacterSheet.java
+++ src/main/java/es/thalesalv/bot/rpg/model/V20CharacterSheet.java
@@ -1,5 +1,7 @@
 package es.thalesalv.bot.rpg.model;
 
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -43,7 +45,7 @@ public class V20CharacterSheet {
     private Integer leadership;
     private Integer streetwise;
     private Integer subterfuge;
-    
+
     private Integer animalKen;
     private Integer crafts;
     private Integer drive;
@@ -54,7 +56,7 @@ public class V20CharacterSheet {
     private Integer performance;
     private Integer stealth;
     private Integer survival;
-    
+
     private Integer academics;
     private Integer computer;
     private Integer finance;
@@ -65,4 +67,39 @@ public class V20CharacterSheet {
     private Integer politics;
     private Integer science;
     private Integer technology;
+
+    /* Vantagens */
+    private String disciplineOne;
+    private Integer disciplineOneLevel;
+    private String disciplineTwo;
+    private Integer disciplineTwoLevel;
+    private String disciplineThree;
+    private Integer disciplineThreeLevel;
+    private String disciplineFour;
+    private Integer disciplineFourLevel;
+    private String disciplineFive;
+    private Integer disciplineFiveLevel;
+    private String disciplineSix;
+    private Integer disciplineSixLevel;
+    
+    private String backgroundOne;
+    private Integer backgroundOneLevel;
+    private String backgroundTwo;
+    private Integer backgroundTwoLevel;
+    private String backgroundThree;
+    private Integer backgroundThreeLevel;
+    private String backgroundFour;
+    private Integer backgroundFourLevel;
+    private String backgroundFive;
+    private Integer backgroundFiveLevel;
+    private String backgroundSix;
+    private Integer backgroundSixLevel;
+    
+    private Integer conscienceConviction;
+    private Integer selfControlInstinct;
+    private Integer courage;
+    
+    /* Other */
+    private String note;
+    private List<String> notes;
 }

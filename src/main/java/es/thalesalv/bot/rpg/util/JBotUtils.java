388e910f1edb8b9e0d02663016677416a442e8a5
Thales Alves <thales.pereira@dell.com>
2019-03-28 16:15:47 -0300

    created method for embed builder

--- src/main/java/es/thalesalv/bot/rpg/util/JBotUtils.java
+++ src/main/java/es/thalesalv/bot/rpg/util/JBotUtils.java
@@ -1,5 +1,6 @@
 package es.thalesalv.bot.rpg.util;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -10,19 +11,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.ResourceUtils;
 
+import net.dv8tion.jda.core.EmbedBuilder;
+
 public class JBotUtils {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(JBotConfig.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(JBotConfig.class);
 
-	public static Properties fetchBotProperties() {
-		Properties properties = new Properties();
-		try {
-			File file = ResourceUtils.getFile("classpath:bot.properties");
-			InputStream in = new FileInputStream(file);
-			properties.load(in);
-		} catch (IOException e) {
-			LOGGER.error(e.getMessage());
-		}
-		return properties;
+    public static Properties fetchBotProperties() {
+        Properties properties = new Properties();
+        try {
+            File file = ResourceUtils.getFile("classpath:bot.properties");
+            InputStream in = new FileInputStream(file);
+            properties.load(in);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return properties;
+    }
+
+    public static EmbedBuilder buildBuilder(EmbedBuilder builder) throws Exception {
+	    try {
+	        builder.setColor(Color.YELLOW);
+            builder.setFooter(JBotConfig.FOOTER_TEXT, JBotConfig.FOOTER_IMG);
+	        return builder;
+	    } catch (Exception e) {
+	        LOGGER.error(e.getMessage());
+	        throw new RuntimeException(e);
+	    }
 	}
 }

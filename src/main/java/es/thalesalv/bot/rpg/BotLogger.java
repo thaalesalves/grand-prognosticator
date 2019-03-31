02277c579c4fa97655692a47c82b5bfbec13ae98
Thales Alves <thales@thalesalv.es>
2019-03-30 21:42:49 -0300

    inicio de implementacao de funcoes de logging

---
+++ src/main/java/es/thalesalv/bot/rpg/BotLogger.java
@@ -0,0 +1,96 @@
+package es.thalesalv.bot.rpg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.thalesalv.bot.rpg.util.JBotConfig;
+import es.thalesalv.bot.rpg.util.JBotUtils;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.core.events.self.SelfUpdateNameEvent;
+import net.dv8tion.jda.core.events.user.update.UserUpdateNameEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class BotLogger extends ListenerAdapter {
+
+	private Guild guild;
+	private MessageChannel channel;
+	private EmbedBuilder builder;
+	private User author;
+	private static final Logger LOGGER = LoggerFactory.getLogger(BotLogger.class);
+
+	private void setGuild(Event event) throws Exception {
+		guild = event.getJDA().getGuildById(JBotConfig.DISCORD_GUILD_ID);
+		channel = guild.getTextChannelById(JBotConfig.DISCORD_LOG_CHANNEL_ID);
+		builder = JBotUtils.buildBuilder(new EmbedBuilder());
+	}
+
+	@Override
+	public void onSelfUpdateName(SelfUpdateNameEvent event) {
+		try {
+			setGuild(event);
+			author = event.getSelfUser();
+			String oldNickname = event.getOldName();
+			String newNickname = event.getNewName();
+
+			builder.setTitle("Refletindo... calculando... logando...");
+			String logContent = "**Usuário:** " + author.getAsMention() + "\n**Data:** "
+					+ JBotUtils.getCurrentDateTime() + "\n**Evento:** " + oldNickname + " alterou o nome para "
+					+ newNickname;
+			builder.setDescription(logContent);
+
+			LOGGER.info(author.getName() + " alterou o nickname de " + oldNickname + " para " + newNickname);
+			channel.sendMessage(builder.build()).complete();
+		} catch (Exception e) {
+			LOGGER.info(e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void onUserUpdateName(UserUpdateNameEvent event) {
+		try {
+			setGuild(event);
+			author = event.getUser();
+			String oldNickname = event.getOldName();
+			String newNickname = event.getNewName();
+
+			builder.setTitle("Refletindo... calculando... logando...");
+			String logContent = "**Usuário:** " + author.getAsMention() + "\n**Data:** "
+					+ JBotUtils.getCurrentDateTime() + "\n**Evento:** " + oldNickname + " alterou o nome para "
+					+ newNickname;
+			builder.setDescription(logContent);
+
+			LOGGER.info(author.getName() + " alterou o nickname de " + oldNickname + " para " + newNickname);
+			channel.sendMessage(builder.build()).complete();
+		} catch (Exception e) {
+			LOGGER.info(e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void onMessageUpdate(MessageUpdateEvent event) {
+		try {
+			setGuild(event);
+			author = event.getAuthor();
+			builder.setTitle("Refletindo... calculando... logando...");
+			String oldMessage = event.getMessage().getContentRaw();
+			MessageChannel messageChannel = event.getChannel();
+			String logContent = "**Usuário:** " + author.getAsMention() + "\n**Data:** "
+					+ JBotUtils.getCurrentDateTime() + "\n**Evento:** " + author.getAsMention()
+					+ " editou uma mensagem em <#" + messageChannel.getId() + ">:\n" + oldMessage;
+			builder.setDescription(logContent);
+
+			LOGGER.info(author.getName() + " editou uma mensagem em " + messageChannel.getName());
+			channel.sendMessage(builder.build()).complete();
+		} catch (Exception e) {
+			LOGGER.info(e.getMessage());
+			throw new RuntimeException(e);
+		}
+	}
+}

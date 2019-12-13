6a36327849864f224dc46c925f8ffd0a7caec79e
Thales Alves <thales@ibm.com>
2019-12-13 11:58:31 -0300

    camada de modelo de banco separada das demais

---
+++ src/main/java/es/thalesalv/bot/rpg/model/entity/CharacterSheetAmaranth.java
@@ -0,0 +1,51 @@
+package es.thalesalv.bot.rpg.model.entity;
+
+import java.io.File;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.thalesalv.bot.rpg.model.Sheet;
+import es.thalesalv.bot.rpg.model.sheet.AttributeSet;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "sheet_amaranth")
+public class CharacterSheetAmaranth extends Sheet {
+
+    private AttributeSet physical;
+    private AttributeSet social;
+    private AttributeSet mental;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CharacterSheetAmaranth.class);
+
+    @Override
+    public PDDocument populateSheet() {
+        try {
+            PDDocument sheet;
+            sheet = PDDocument.load(new File(""));
+            return sheet;
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected String gameName() {
+        return "Amaranth";
+    }
+
+    @Override
+    public String generateFileName() {
+        return sheetSaveDir + "/" + this.getPlayerName() + "_" + this.getPlayerId().intValue() + "_"
+                + this.getCharacterName() + this.gameName() + "_" + ".pdf".replace(" ", "_");
+    }
+}
